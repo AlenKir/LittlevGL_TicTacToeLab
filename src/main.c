bc89a44b001949795cdf2dc6b0d8af3bc943f1c6
AlenKir <kirillova.alena@gmail.com>
2020-04-06 22:37:23 +0300

    cleaned up a bit

--- src/main.c
+++ src/main.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <time.h>
 #include "lvgl/lvgl.h"
-#include "lv_tutorial_hello_world.h"
 
 //-----------------------------------------------------------------------------
 // Display wrapper structures and functions
@@ -55,44 +54,6 @@ static void event_handler(lv_obj_t * obj, lv_event_t event)
         }
     }
 }
-
-void lv_ex_btn_1(void)
-{
-//    lv_obj_t * label;
-//
-//    lv_obj_t * btn1 = lv_btn_create(lv_scr_act(), NULL);
-//    lv_obj_set_event_cb(btn1, event_handler);
-//    lv_obj_align(btn1, NULL, LV_ALIGN_CENTER, 0, -40);
-//
-//    label = lv_label_create(btn1, NULL);
-//    lv_label_set_text(label, " ");
-
-//    lv_obj_t * btn2 = lv_btn_create(lv_scr_act(), NULL);
-//    lv_obj_set_event_cb(btn2, event_handler);
-//    lv_obj_align(btn2, NULL, LV_ALIGN_CENTER, 0, 40);
-//    lv_btn_set_toggle(btn2, true);
-//    lv_btn_toggle(btn2);
-//    lv_btn_set_fit2(btn2, LV_FIT_NONE, LV_FIT_TIGHT);
-//
-//    label = lv_label_create(btn2, NULL);
-//    lv_label_set_text(label, "Toggled");
-}
-
-//void lv_tutorial_hello_world(void)
-//{
-//    lv_obj_t * scr = lv_disp_get_scr_act(NULL);     /*Get the current screen*/
-//
-//    /*Create a Label on the currently active screen*/
-//    lv_obj_t * label1 =  lv_label_create(scr, NULL);
-//
-//    /*Modify the Label's text*/
-//    lv_label_set_text(label1, "Hello world!");
-//
-//    /* Align the Label to the center
-//     * NULL means align on parent (which is the screen now)
-//     * 0, 0 at the end means an x, y offset after alignment*/
-//    lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
-//}
 
 /**
  * Initialize SDL and demo display.
@@ -240,42 +201,6 @@ int demo_display_fill_rect(demo_display_t *demo_disp, int x1, int y1, int x2, in
     return 0;
 }
 
-/**
- * Helper function to draw filled rectangle with demo_display_fill_rect.
- */
-//int demo_display_draw_rect(demo_display_t *demo_disp, int x1, int y1, int x2, int y2, uint16_t color) {
-//    // check rectangle coordinates
-//    int tmp;
-//    x1 = x1 < 0 ? 0 : x1;
-//    x1 = x1 >= demo_disp->width ? demo_disp->width - 1 : x1;
-//    x2 = x2 < 0 ? 0 : x2;
-//    x2 = x2 >= demo_disp->width ? demo_disp->width - 1 : x2;
-//    y1 = y1 < 0 ? 0 : y1;
-//    y1 = y1 >= demo_disp->height ? demo_disp->height - 1 : y1;
-//    y2 = y2 < 0 ? 0 : y2;
-//    y2 = y2 >= demo_disp->height ? demo_disp->height - 1 : y2;
-//    if (x1 > x2) {
-//        tmp = x2;
-//        x2 = x1;
-//        x1 = tmp;
-//    }
-//    if (y1 > y2) {
-//        tmp = y2;
-//        y2 = y1;
-//        y1 = tmp;
-//    }
-//
-//    // allocate and fill array with colors
-//    int rect_size = (x2 - x1 + 1) * (y2 - y1 + 1);
-//    uint16_t colors[rect_size];
-//    for (int i = 0; i < rect_size; i++) {
-//        colors[i] = color;
-//    }
-//
-//    // render rectangle
-//    return demo_display_fill_rect(demo_disp, x1, y1, x2, y2, colors);
-//}
-
 //-----------------------------------------------------------------------------
 // LittlevGL driver port
 //-----------------------------------------------------------------------------
@@ -381,81 +306,6 @@ int demo_lvgl_deinit(demo_lvgl_disp_t *demo_lvgl_disp) {
     return 0;
 }
 
-//-----------------------------------------------------------------------------
-// Demo struct and function
-//-----------------------------------------------------------------------------
-
-typedef struct {
-    lv_obj_t *scr;
-    lv_style_t scr_style;
-
-    lv_obj_t *time_label;
-    lv_style_t time_label_style;
-
-    lv_obj_t *reset_btn;
-    lv_obj_t *reset_btn_label;
-
-    int ms_offset;
-} demo_app_t;
-
-int demo_app_init(demo_app_t *demo);
-
-/**
- * Button callback.
- */
-static void demo_app_reset_btn_cb(lv_obj_t *reset_btn, lv_event_t event)
-{
-    demo_app_t *app = reset_btn->user_data;
-    switch (event) {
-    case LV_EVENT_PRESSED:
-        app->time_label_style.body.main_color = LV_COLOR_RED;
-        app->time_label_style.body.grad_color = LV_COLOR_RED;
-        app->ms_offset = SDL_GetTicks();
-        break;
-    case LV_EVENT_RELEASED:
-        app->time_label_style.body.main_color = lv_style_pretty_color.body.main_color;
-        app->time_label_style.body.grad_color = lv_style_pretty_color.body.grad_color;
-
-        break;
-    }
-}
-
-/**
- * Initialize demo application.
- */
-//int demo_app_init(demo_app_t *demo) {
-//
-//    // get active screen
-//    demo->scr = lv_scr_act();
-//    // create screen style
-//    lv_style_copy(&demo->scr_style, &lv_style_plain);
-//    demo->scr_style.body.main_color = LV_COLOR_BLUE;
-//    demo->scr_style.body.grad_color = lv_color_hex(0xffd83c);
-//    lv_obj_set_style(demo->scr, &demo->scr_style);
-//
-//    // create timer label
-//    demo->time_label = lv_label_create(demo->scr, NULL);
-//    lv_style_copy(&demo->time_label_style, &lv_style_pretty_color);
-//    lv_label_set_long_mode(demo->time_label, LV_LABEL_LONG_CROP);
-//    lv_obj_set_size(demo->time_label, 100, 30);
-//    lv_obj_set_pos(demo->time_label, 70, 50);
-//    lv_label_set_align(demo->time_label, LV_LABEL_ALIGN_CENTER);
-//    lv_label_set_style(demo->time_label, LV_LABEL_STYLE_MAIN, &demo->time_label_style);
-//    lv_label_set_body_draw(demo->time_label, 1);
-//    lv_label_set_text(demo->time_label, "00:00");
-//
-//    // create button
-//    demo->reset_btn = lv_btn_create(demo->scr, NULL);
-//    lv_obj_set_size(demo->reset_btn, 120, 60);
-//    lv_obj_set_pos(demo->reset_btn, 60, 200);
-//    demo->reset_btn_label = lv_label_create(demo->reset_btn, NULL);
-//    lv_label_set_text(demo->reset_btn_label, "RESET");
-//    demo->reset_btn->user_data = demo;
-//    lv_obj_set_event_cb(demo->reset_btn, demo_app_reset_btn_cb);
-//
-//    return 0;
-//}
-
 int main(int n, char **args) {
     int err;
     int ret_code = 0;
@@ -476,15 +326,6 @@ int main(int n, char **args) {
         return -1;
     }
 
-    // initialize demo application
-//    demo_app_t app;
-//    err = demo_app_init(&app);
-//    if (err) {
-//        return err;
-//    }
-
-//    lv_ex_btn_1();
-
     symbol = true;
 
     // coord caclulation
@@ -502,26 +343,13 @@ int main(int n, char **args) {
         else
         	coord_x = coord_x + 105;
         labels[i] = lv_label_create(buttons[i], NULL);
-//        char str[2];
-//        int digit = i+1;
-//        str[0] = digit + '0';
-//        str[1] = 0;
         lv_label_set_text(labels[i], " ");
     }
-
-//    label1 = lv_label_create(btn1, NULL);
-//    lv_label_set_text(label1, " ");
-
-    // some application and LittlevGL logic
 
     uint32_t t, t_prev;
     t_prev = SDL_GetTicks();
 
     while (1) {
-        // update app label
-//        uint32_t current_msec = SDL_GetTicks() - app.ms_offset;
-//        lv_label_set_text_fmt(app.time_label, "%02i:%05i", current_msec / 60000, current_msec % 60000);
-
         // read SDL events
         err = demo_display_process_events(&demo_disp);
         if (err) {
